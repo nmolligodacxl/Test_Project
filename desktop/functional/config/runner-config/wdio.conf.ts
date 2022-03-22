fa244bae54f54e424b9145991a9c16bc4fb28161
nmolligodacxl <nmolligodacxl@cxloyalty.com>
2022-03-22 15:10:23 -0400

    added allure step reporting config

--- desktop/functional/config/runner-config/wdio.conf.ts
+++ desktop/functional/config/runner-config/wdio.conf.ts
@@ -159,7 +159,7 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec',['allure', {outputDir: './desktop/functional/reports/allure-results'}]],
+    reporters: ['spec',['allure', {outputDir: './desktop/functional/reports/allure-results', disableWebdriverStepsReporting: true}]],
 
 
     
