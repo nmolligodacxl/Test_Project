cd9b0012d6ba574e68ed7172d07cf7d57ca3d3fd
nmolligodacxl <nmolligodacxl@cxloyalty.com>
2022-03-22 14:48:21 -0400

    .gitignore is now working

--- inversify.config.ts
+++
@@ -1,7 +0,0 @@
-import { Container } from 'inversify';
-import {FlightSearch} from './desktop/functional/dsl/flight-search.dsl'
-
-const myContainer = new Container();
-myContainer.bind<FlightSearch>(FlightSearch.CONTAINER_ID).to(FlightSearch);
-
-export {myContainer};
